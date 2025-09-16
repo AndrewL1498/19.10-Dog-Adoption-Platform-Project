b0ade5e65c385fce89fe76b37547f89d65350dab
AndrewL1498 <andrewlindsey498@gmail.com>
2025-09-16 00:13:50 -0500

    Update user schema with validations and password hashing

--- models/UserModel.js
+++ models/UserModel.js
@@ -6,23 +6,33 @@ const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, "Username is required"],
         unique: true,
         lowercase: true,
+        minlength: [3, "Username must be at least 3 characters long"],
+        maxlength: [20, "Username cannot exceed 20 characters"],
+        match: [/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores"]
     },
     password: {
         type: String,
-        required: true,
-        minlength: 6,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"],
+        match: [/[!@#$%^&*(),.?":{}|<>]/, "Password must contain at least one special character"]
     },
 });
 
-userSchema.pre('save', async function(next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
+userSchema.pre('save', async function(next) { // pre means this function will run before saving a user to the database
+    if (!this.isModified('password')) return next(); // if the password is not modified, skip hashing
+    const salt = await bcrypt.genSalt(); // generate a salt
+    this.password = await bcrypt.hash(this.password, salt); // hash the password with the salt
     next();
 });
 
-const user = mongoose.model('user', userSchema);
+userSchema.methods.isValidPassword = async function(password) { //.method adds a method to the userSchema
+  return await bcrypt.compare(password, this.password); // compare the provided password with the hashed password in the database
+};
 
-module.exports = user;
+
+const User = mongoose.model('User', userSchema); // mongoose.model tells mongoose to create a collection named 'users' in the database and use the userSchema to define the structure of the documents in that collection
+
+module.exports = User;
